046771d916d741940bad3a3f7f68b0cf7f9ea800
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-05-16 10:41:07 +0200

    Added product stock counter

--- src/app/components/primary-button/primary-button.ts
+++ src/app/components/primary-button/primary-button.ts
@@ -6,7 +6,7 @@ import { Component, input, output } from '@angular/core';
   template: `
     <button class="bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacitiy-90" 
           (click)="handleButtonClick()">
-      {{ label( )}}
+      {{ label()}}
     </button>
   `,
   styles: ``
--- src/app/pages/products-list/product-card/product-card.ts
+++ src/app/pages/products-list/product-card/product-card.ts
@@ -14,15 +14,23 @@ import { PrimaryButton } from "../../../components/primary-button/primary-button
           class="w-[200px] h-[100px] object-contain"
           />
         </div>
+        
           <div class="flex flex-col mt-2">
             <span class="text-md font-bold"> {{ product().title }} </span>
             <span class="text-sm"> {{ '€' + product().price }} </span>
             <app-primary-button label="Add to Cart" class="mt-3" />
-          </div>         
-        </div>
-      
+          </div>    
+          
+      <span
+        class="absolute top-2 right-3 text-sm font-bold"
+        [class]="product().stock ? 'text-green-500' : 'text-red-500'"
+      >
+        @if (product().stock) {
+        {{ product().stock }} left } @else { Out of stock }
+      </span>
+    </div>
   `,
-  styles: ``
+  styles: ``,
 })
 export class ProductCard {
   product = input.required<Product>();
